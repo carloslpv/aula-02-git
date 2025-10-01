afe8b9a03018f8a020146b6202ad2293540f4c9c
Carlos Vieira <carloscelpv@outlook.com>
2025-10-01 20:37:54 -0300

    fix: remove método updateHistoricoManutencao

--- src/main/java/monitoramento_prensas/services/HistoricoManutencaoService.java
+++ src/main/java/monitoramento_prensas/services/HistoricoManutencaoService.java
@@ -91,37 +91,6 @@ public class HistoricoManutencaoService {
     }
 
     /**
-     * Atualiza o cadastro de um {@link HistoricoManutencao}.
-     *
-     * @param dto
-     * @throws ObjetoNaoEncontradoException
-     * @throws PersistenceException
-     */
-    public void updateHistoricoManutencao(final HistoricoManutencaoDTO dto) throws ObjetoNaoEncontradoException, PersistenceException {
-        try {
-            final HistoricoManutencao historicoManutencao = this.getHistoricoManutencao(dto.id());
-            Maquina maquina;
-            if(historicoManutencao.getMaquina().getId() != dto.idMaquina()){
-                maquina = this.maquinaService.getMaquina(dto.idMaquina());
-                historicoManutencao.setMaquina(maquina);
-            }
-            HistoricoFalhas historicoFalhas;
-            if(historicoManutencao.getHistoricoFalha() != null && historicoManutencao.getHistoricoFalha().getId() !=
-                    dto.idHistoricoFalha()) {
-                historicoFalhas = this.historicoFalhasService.getFalha(dto.idHistoricoFalha());
-                historicoManutencao.setHistoricoFalha(historicoFalhas);
-            }
-            historicoManutencao.setTipoManutencao(dto.tipoManutencao());
-            historicoManutencao.setAcaoRealizada(dto.acaoRealizada());
-            historicoManutencao.setDataHoraManutencao(dto.dataHoraManutencao());
-            historicoManutencao.setTempoManutencaoMin(dto.tempoManutencaoMin());
-            this.historicoManutencaoRepository.save(historicoManutencao);
-        } catch (Exception e) {
-            throw new PersistenceException("Não foi possível atualizar o cadastro do histórico de manutenção. Verifique!");
-        }
-    }
-
-    /**
      * Remove o cadastro de um {@link HistoricoManutencao}.
      *
      * @param id
